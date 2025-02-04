9c2f5cec073cdbbdfd43e6adef02aa6e68f9a3db
Utkarsh Kumar Upadhyay <ukupadhyay6789@gmail.com>
2025-02-05 00:52:26 +0530

    imagekit provider done

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,40 +1,44 @@
-"use client"
+"use client";
 
 import React from "react";
 import { ImageKitProvider, IKImage } from "imagekitio-next";
+import { SessionProvider } from "next-auth/react";
 
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-// /**
-//  * Authenticates the user by making a request to the authentication API endpoint.
-//  *
-//  * @returns {Promise<{signature: string, expire: number, token: string}>} 
-//  *          A promise that resolves with an object containing the authentication 
-//  *          signature, expiration time, and token.
-//  * @throws {Error} Throws an error if the request fails or if the response is not OK.
-//  */
+  // /**
+  //  * Authenticates the user by making a request to the authentication API endpoint.
+  //  *
+  //  * @returns {Promise<{signature: string, expire: number, token: string}>}
+  //  *          A promise that resolves with an object containing the authentication
+  //  *          signature, expiration time, and token.
+  //  * @throws {Error} Throws an error if the request fails or if the response is not OK.
+  //  */
 
-    const authenticator = async () => {
-        try {
-          const res = await fetch("/api/imagekit-auth");
-      
-          if (!res.ok) {
-            throw new Error("Failed to authenticate user");
-          }
-      
-          return res.json(); // this res contain token , expiry and signature
-        } catch (error) {
-          throw error;
-        }
-      };
-      
+  const authenticator = async () => {
+    try {
+      const res = await fetch("/api/imagekit-auth");
 
+      if (!res.ok) {
+        throw new Error("Failed to authenticate user");
+      }
+      return res.json(); // this res contain token , expiry and signature
+    } catch (error) {
+      throw error;
+    }
+  };
 
-    return (
-        <ImageKitProvider urlEndpoint={urlEndpoint!} publicKey={publicKey!}>
-            {children}
-        </ImageKitProvider>
-    );
+  return (
+    <SessionProvider refetchInterval={5*60}>
+      <ImageKitProvider
+      urlEndpoint={urlEndpoint}
+      publicKey={publicKey}
+      authenticator={authenticator}
+    >
+      {children}
+    </ImageKitProvider>
+    </SessionProvider>
+  );
 }
